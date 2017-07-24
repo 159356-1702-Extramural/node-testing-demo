8e318fc7b95a10fa174a7225251e67642c54f50c
Tim Ackroyd <tim@customd.com>
2017-07-24 22:52:38 +1200

    Reduce test coverage

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 
 var config = {
     name        : "Settlers of Catan",
-    maxPlayers  : 4
+    maxPlayers  : 4,
+    minPlayers  : 3
 };
 
 // Gets the name of the game
@@ -17,7 +18,15 @@ function getMaxPlayers()
     return config.maxPlayers;
 }
 
+// Gets the min number of players
+// NOTE: This function not covered by unit tests
+function getMinPlayers()
+{
+    return config.minPlayers;
+}
+
 module.exports = {
-  getGameName   : getGameName,
-  getMaxPlayers : getMaxPlayers
+    getGameName   : getGameName,
+    getMaxPlayers : getMaxPlayers,
+    getMinPlayers : getMinPlayers
 };
