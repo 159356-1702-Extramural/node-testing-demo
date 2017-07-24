fd3b1e7283cb8d44bb6f6fe28d794936b13f5104
Tim Ackroyd <tim@customd.com>
2017-07-24 22:13:59 +1200

    Adding instanbul for code coverage

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# Testing Unit Testing
+
 [![Build Status](https://travis-ci.org/159356-1702-Extramural/node-testing-demo.svg?branch=master)](https://travis-ci.org/159356-1702-Extramural/node-testing-demo)
--- game.js
+++ game.js
@@ -1,15 +1,23 @@
 
 var config = {
-    name : "Settlers of Catan"
+    name        : "Settlers of Catan",
+    maxPlayers  : 4
 };
 
+// Gets the name of the game
 function getGameName()
 {
     return config.name; 
 } 
 
-
+// Gets the max number of players
+// NOTE: This function not covered by unit tests
+function getMaxPlayers()
+{
+    return config.maxPlayers;
+}
 
 module.exports = {
-  getGameName: getGameName
-}
+  getGameName   : getGameName,
+  getMaxPlayers : getMaxPlayers
+};
