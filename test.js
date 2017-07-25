3d5315ad4377e0cb8101ffd4b345173a1e2a82a1
Tim Ackroyd <tim@customd.com>
2017-07-25 22:25:32 +1200

    Testing Ava

--- test.js
+++ test.js
@@ -1,35 +1,33 @@
+var test = require('ava');
 var game = require('./game.js');
-
-var assert = require('assert');
 
 var tim = {
     name : "Tim"
 };
 
 // Test Tim...
-describe('Tim Test', function() {
-    describe('#name', function() {       
-        it('Tim\'s name should be equal to "Tim"', function() {
-            assert.equal("Tim", tim.name);
-        });
-        it('Tim\'s name should not be Jason', function() {
-            assert.notEqual("Jason", tim.name);
-        });
-    });
+test('Tim\'s name should be equal to "Tim"', function(t) {
+    t.true("Tim" == tim.name);
+});
+
+test('Tim\'s name should not be Jason', function(t) {
+    t.true("Jason" != tim.name);
 });
 
 // Test Game...
-describe('Game Test', function() {
-    describe('#game', function() {
-        it('The game name should be: Settlers of Catan', function() {
-            assert.equal(game.getGameName(), 'Settlers of Catan');
-        });
-    });
+test('The game name should be: Settlers of Catan', function(t) {
+    t.true(game.getGameName() == 'Settlers of Catan');
 });
 
-// Failing Test
-describe('Test Addition', function() {
-    it('1 + 1 should equal 2', function() {
-        assert.equal(2, 1 + 1);
-    });
+// Failing test..
+test('1 + 1 should equal 2', function(t) {
+    t.true(1 + 1 == 3);
 });
+
+
+// // Failing Test
+// describe('Test Addition', function() {
+//     it('1 + 1 should equal 2', function() {
+//         assert.equal(2, 1 + 1);
+//     });
+// });
