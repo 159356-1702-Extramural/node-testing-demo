35307352f5d805f6c058103ff43c8a266714b5fe
Craig Walker <craig@coded.co.nz>
2017-08-07 13:26:39 +1200

    Shifted test files to test files folder

---
+++ test/test.js
@@ -0,0 +1,36 @@
+var game = require('../game.js');
+
+var assert = require('assert');
+var selenium = require('selenium-webdriver')
+var tim = {
+    name : "Tim"
+};
+
+// Test Tim...
+describe('Tim Test', function() {
+    describe('#name', function() {       
+        it('Tim\'s name should be equal to "Tim"', function() {
+            assert.equal("Tim", tim.name);
+        });
+        it('Tim\'s name should not be Jason', function() {
+            assert.notEqual("Jason", tim.name);
+        });
+    });
+});
+
+// Test Game...
+describe('Game Test', function() {
+    describe('#game', function() {
+        it('The game name should be: Settlers of Catan', function() {
+            assert.equal(game.getGameName(), 'Settlers of Catan');
+        });
+    });
+});
+
+// Failing Test
+describe('Test Addition', function() {
+    it('1 + 1 should equal 2', function() {
+        assert.equal(2, 1 + 1);
+    });
+});
+
