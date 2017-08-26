7067e81ef9ffc26f497dad31fb56de0fb588f410
Craig Walker <craig@coded.co.nz>
2017-08-26 22:46:04 +1200

    changed nightly tests

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -5,8 +5,12 @@ set -ev
 # echo ${TRAVIS_EVENT_TYPE};
 #    + returns 'api' when manually triggering build
 #    + returns 'push' when pushing to remote branch
-
+echo "NIghtly tests running";
 if [ "${TRAVIS_EVENT_TYPE}" = "cron" ]; then
     # run selenium tests here
     echo test;
+fi
+if [ "true" = "true" ]; then
+    npm test
+    echo "npm tests running";
 fi
